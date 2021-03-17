1cf5d7f023e906e7adf03a9fe76d4447be6a9ee7
hechth <helge.hecht@gmx.de>
2021-03-17 16:27:26 +0100

    Added test checking for 'None' spectrum

--- tests/test_fidelity.py
+++ tests/test_fidelity.py
@@ -17,3 +17,17 @@ def test_fidelity(tolerance, expected_score, expected_matches):
     score = fidelity.pair(spectrum_1, spectrum_2)
     assert score["score"] == pytest.approx(expected_score)
     assert score["matches"] == expected_matches
+
+
+def test_none_exception():
+    reference = None
+    query = Spectrum(mz=numpy.array([100, 200, 290, 490, 510], dtype="float"),
+                     intensities=numpy.array([0.1, 0.2, 1.0, 0.3, 0.4],
+                                             dtype="float"))
+
+    fidelity = Fidelity(tolerance=0.0)
+    with pytest.raises(AttributeError) as exception:
+        fidelity.pair(reference, query)
+
+    message = exception.value.args[0]
+    assert message == "'NoneType' object has no attribute 'peaks'"
