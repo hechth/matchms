69cc7552767157de2a80e3bf484b0831d6662ac0
hechth <helge.hecht@gmx.de>
2021-03-17 13:51:25 +0100

    Implemented basic test case for fidelity score

--- tests/test_fidelity.py
+++ tests/test_fidelity.py
@@ -1,5 +1,19 @@
 import numpy
 import pytest
 from matchms import Spectrum
-from matchms import calculate_scores
 from matchms.similarity import Fidelity
+
+
+@pytest.mark.parametrize("tolerance, expected_score, expected_matches", [(0.1, 0.35, 3)])
+def test_fidelity(tolerance, expected_score, expected_matches):
+    """Compare output fidelity distance with scores computed via daphnis package implementation."""
+    spectrum_1 = Spectrum(mz=numpy.array([100, 200, 300, 500, 510], dtype="float"),
+                          intensities=numpy.array([0.1, 0.2, 1.0, 0.3, 0.4], dtype="float"))
+
+    spectrum_2 = Spectrum(mz=numpy.array([100, 200, 290, 490, 510], dtype="float"),
+                          intensities=numpy.array([0.1, 0.2, 1.0, 0.3, 0.4], dtype="float"))
+
+    fidelity = Fidelity(tolerance=tolerance)
+    score = fidelity.pair(spectrum_1, spectrum_2)
+    assert score["score"] == pytest.approx(expected_score)
+    assert score["matches"] == expected_matches
