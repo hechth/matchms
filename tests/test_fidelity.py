10c43ac8fa18bb7a2f139cdc1400522ff918b5bd
hechth <helge.hecht@gmx.de>
2021-03-18 08:52:58 +0100

    Additional test cases for score and multiple matches

--- tests/test_fidelity.py
+++ tests/test_fidelity.py
@@ -4,30 +4,45 @@ from matchms import Spectrum
 from matchms.similarity import Fidelity
 
 
-@pytest.mark.parametrize("tolerance, expected_score, expected_matches", [(0.1, 0.1225, 3)])
-def test_fidelity(tolerance, expected_score, expected_matches):
-    """Compare output fidelity distance with scores computed via daphnis package implementation."""
-    spectrum_1 = Spectrum(mz=numpy.array([100, 200, 300, 500, 510], dtype="float"),
-                          intensities=numpy.array([0.1, 0.2, 1.0, 0.3, 0.4], dtype="float"))
+@pytest.fixture(autouse=True, scope='function')
+def none_spectrum():
+    return None
 
-    spectrum_2 = Spectrum(mz=numpy.array([100, 200, 290, 490, 510], dtype="float"),
-                          intensities=numpy.array([0.1, 0.2, 1.0, 0.3, 0.4], dtype="float"))
+
+@pytest.fixture
+def spectrum1():
+    return Spectrum(mz=numpy.array([100, 200, 300, 500, 510], dtype="float"),
+                    intensities=numpy.array([0.1, 0.2, 1.0, 0.3, 0.4], dtype="float"))
+
+
+@pytest.fixture
+def spectrum2():
+    return Spectrum(mz=numpy.array([100, 200, 290, 490, 510], dtype="float"),
+                    intensities=numpy.array([0.1, 0.2, 1.0, 0.3, 0.4], dtype="float"))
+
+
+@pytest.fixture
+def empty_spectrum():
+    return Spectrum(mz=numpy.array([], dtype="float"),
+                    intensities=numpy.array([], dtype="float"))
+
+
+@pytest.mark.parametrize("tolerance, expected_score, expected_matches", [(0.1, 0.1225, 3), (20, 1.0, 5)])
+def test_fidelity_score(tolerance, expected_score, expected_matches, spectrum1, spectrum2):
+    """Compare output fidelity distance with scores computed with manual calculator."""
 
     fidelity = Fidelity(tolerance=tolerance)
-    score = fidelity.pair(spectrum_1, spectrum_2)
+    score = fidelity.pair(spectrum1, spectrum2)
     assert score["score"] == pytest.approx(expected_score)
     assert score["matches"] == expected_matches
 
 
-def test_none_exception():
-    reference = None
-    query = Spectrum(mz=numpy.array([100, 200, 290, 490, 510], dtype="float"),
-                     intensities=numpy.array([0.1, 0.2, 1.0, 0.3, 0.4],
-                                             dtype="float"))
+def test_none_exception(none_spectrum, spectrum1):
+    """ Test for exception being thrown if the reference or query spectrum is none. """
 
     fidelity = Fidelity(tolerance=0.0)
     with pytest.raises(AttributeError) as exception:
-        fidelity.pair(reference, query)
+        fidelity.pair(none_spectrum, spectrum1)
 
     message = exception.value.args[0]
     assert message == "'NoneType' object has no attribute 'peaks'"
